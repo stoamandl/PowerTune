9ae9c835b5a65c5cc6b6d8291097b4c7b447d361
BastianGschrey <b.gschrey@gmail.com>
2015-11-10 22:16:26 +0100

    Backgroundworker stopping working. UI Change from Backgroundworker working

--- PowerTune/MainWindow.xaml.cs
+++ PowerTune/MainWindow.xaml.cs
@@ -23,13 +23,28 @@ namespace PowerTune
     {
         BackgroundWorker workerGetAdvData;
 
+
+
+
         public MainWindow()
         {
+
+            ClsGetAdvData getAdvData = new ClsGetAdvData(); //create a new instance of ClsGetAdvData Class
+
+
+
+            //initialize the workerGetAdvData Background Worker
             workerGetAdvData = new BackgroundWorker();
+            workerGetAdvData.WorkerSupportsCancellation = true;
+            workerGetAdvData.WorkerReportsProgress = true;
+
+
+            workerGetAdvData.DoWork += new DoWorkEventHandler(getAdvData.request_DoWork);
+            workerGetAdvData.ProgressChanged += new ProgressChangedEventHandler(request_ProgressChanged); 
 
             InitializeComponent();
-            sbStatus.Text = "Footer";
-
+            sbStatus.Text = "Offline";
+            
 
 
             //Load Configuration from settings stored in user profile
@@ -57,18 +72,12 @@ namespace PowerTune
         private void MenuItem_Click_Serial_Start(object sender, RoutedEventArgs e)
         {
 
-            ClsGetAdvData getAdvData = new ClsGetAdvData(); //create a new instance of ClsGetAdvData Class
-
             ClsComSettingMain clsComSettingMain = new ClsComSettingMain();
 
             clsComSettingMain.comport = libs.clsComSettings.strSelectCom;
             clsComSettingMain.baudRate = libs.clsComSettings.strSelectedBaud;
 
-            workerGetAdvData.DoWork += new DoWorkEventHandler(getAdvData.request_DoWork);
-            //workerGetAdvData.RunWorkerCompleted += new RunWorkerCompletedEventHandler(workerGetAdvData_RunWorkerCompleted);
 
-            workerGetAdvData.WorkerReportsProgress = true;
-            workerGetAdvData.WorkerSupportsCancellation = true;
 
 
             if (libs.clsComSettings.strSelectCom == "" || libs.clsComSettings.strSelectedBaud == 0)
@@ -79,18 +88,41 @@ namespace PowerTune
             }
             else
             {
-                workerGetAdvData.RunWorkerAsync(clsComSettingMain);
+                if (!workerGetAdvData.IsBusy)
+                {
+                    workerGetAdvData.RunWorkerAsync(clsComSettingMain);
+                }
             }
         }
 
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_Serial_Stop(object sender, RoutedEventArgs e)
         {
-            if (workerGetAdvData.IsBusy)
+            if (workerGetAdvData.IsBusy) //check if worker is running
             {
                 workerGetAdvData.CancelAsync();
             }
         }
+
+
+        public void request_ProgressChanged(object sender, ProgressChangedEventArgs e) //this function is need to set statusbartext
+        {
+            
+
+            if(e.ProgressPercentage == 0)
+            {
+                sbStatus.Text = "Logging active...";
+            }
+            else
+            {
+                sbStatus.Text = "Offline";
+            }
+
+
+            // parser could be triggered here..?
+        }
     }
+
+
 }
 
 
@@ -101,8 +133,15 @@ class ClsGetAdvData
     //open Serial Port with settings from clsComSettings class
     byte[] adv_request = { 0xF0, 0x02, 0x0D };   //Command for requesting advanced sensor data from PFC
 
+
+
     public void request_DoWork(object sender, DoWorkEventArgs e)
     {
+        var worker = (BackgroundWorker)sender;
+
+        worker.ReportProgress(0);
+
+
         ClsComSettingMain clsComSettingMain = (ClsComSettingMain)e.Argument;
         string comPort = clsComSettingMain.comport;
         int baudRate = clsComSettingMain.baudRate;
@@ -114,8 +153,9 @@ class ClsGetAdvData
 
             while (true)
             {
-                if (e.Cancel)
+                if (worker.CancellationPending)
                 {
+                    worker.ReportProgress(100);
                     e.Cancel = true;
                     break;
                 }
